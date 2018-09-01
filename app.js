cb13d764d2bbd761c908049e4ff012f024f51695
SaifChamsi <saif.chamsi99@gmail.com>
2018-09-01 23:50:00 +0100

    script the EventListener

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
 var scores, roundScore,activePlayer,dice;
 
 scores =[0,0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
+           // Initializing the scores
+document.getElementById('score-0').textContent = 0 ;
+document.getElementById('score-1').textContent = 0 ;
+document.getElementById('current-0').textContent = 0 ;
+document.getElementById('current-1').textContent = 0 ;
 
-dice = Math.floor(Math.random()*6)+1;
-console.log(dice);
+           //hide the Dice in the beginning 
+document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('#score-0').textContent= dice;
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    //1.Random Number
+    var dice = Math.floor(Math.random()*6)+1;console.log(dice);
+    //2.Display the result
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-'+dice+'.png';
+    //3. update the round score if the rolled number was not a 1 
+    if (dice !== 1) {
+        //Add Score
+        roundScore+=dice;
+        document.querySelector('#current-'+activePlayer).textContent= roundScore;
+    }
+
+    else{
+        //Change the player if the dice in (1)
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    }
+});
