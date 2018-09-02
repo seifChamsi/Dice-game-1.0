5c0d87da2f4c8697080e4a8e07744e70e2627291
SaifChamsi <saif.chamsi99@gmail.com>
2018-09-02 20:24:57 +0100

    fixing the bug of activePlayer

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var scores, roundScore,activePlayer,dice;
 
 scores =[0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
            // Initializing the scores
 document.getElementById('score-0').textContent = 0 ;
 document.getElementById('score-1').textContent = 0 ;
@@ -28,7 +28,33 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     }
 
     else{
-        //Change the player if the dice in (1)
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        //Next player
+       nextPlayer();
     }
 });
+
+function nextPlayer() {
+    activePlayer == 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = "0"
+    document.getElementById('current-1').textContent = "0"
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    
+    document.querySelector('.dice').style.display = 'none';        
+}
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    //1.add the current score to global score 
+    scores[activePlayer] += roundScore;
+    //2. update the User interface
+    document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer]
+    //check if the player won the game 
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name-'+activePlayer).textContent="You win :D"
+    }
+
+    //NextPlayer
+    nextPlayer();
+})  
